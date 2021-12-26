cf5cbd6a1792966744521ab5798383b6b2954f89
Giovanni d'Ario <giovanni.dario@gmail.com>
2021-12-26 19:54:26 +0100

    Add another test to the brute force inversion.

--- course1/test_ch3.py
+++ course1/test_ch3.py
@@ -6,3 +6,6 @@ from ch3 import brute_force_inversions
 class TestBruteForceInversions(unittest.TestCase):
     def test_brute_force_inversions(self):
         self.assertEqual(brute_force_inversions([1, 3, 5, 2, 4, 6]), 3)
+
+    def test_brute_inversions2(self):
+        self.assertEqual(brute_force_inversions([5, 4, 3, 2, 1]), 10)
