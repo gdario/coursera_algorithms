96d457abba6555d80fb9a388b49313bfda5f8723
Giovanni d'Ario <giovanni.dario@gmail.com>
2021-12-27 10:43:31 +0100

    Add sort_and_count_inv and its tests.

--- course1/ch3.py
+++ course1/ch3.py
@@ -6,3 +6,49 @@ def brute_force_inversions(array):
             if array[i] > array[j]:
                 n_inversions += 1
     return n_inversions
+
+
+def merge_and_count_split_inv(left, right):
+    """Merge the left and right halves in a mergesort."""
+    left_length = len(left)
+    right_length = len(right)
+    output_length = left_length + right_length
+    output = [0]*output_length
+
+    i, j, split_inv = 0, 0, 0
+
+    for k in range(output_length):
+        if i < left_length and j < right_length:
+            if left[i] <= right[j]:
+                output[k] = left[i]
+                i += 1
+            else:
+                output[k] = right[j]
+                j += 1
+                split_inv += left_length - i
+        elif i < left_length and j == right_length:
+            output[k] = left[i]
+            i += 1
+        else:
+            output[k] = right[j]
+            j += 1
+            split_inv += left_length - i
+
+    return output, split_inv
+
+
+def sort_and_count_inv(x):
+    """Sorts a vector in O(nlogn)."""
+    input_length = len(x)
+    if input_length <= 1:
+        return x, 0
+
+    midpoint = input_length // 2
+    left = x[:midpoint]
+    right = x[midpoint:]
+
+    sorted_left, left_inv = sort_and_count_inv(left)
+    sorted_right, right_inv = sort_and_count_inv(right)
+
+    merged, split_inv = merge_and_count_split_inv(sorted_left, sorted_right)
+    return (merged, left_inv + right_inv + split_inv)
--- course1/test_ch3.py
+++ course1/test_ch3.py
@@ -1,6 +1,7 @@
 import unittest
 
 from ch3 import brute_force_inversions
+from ch3 import sort_and_count_inv
 
 
 class TestBruteForceInversions(unittest.TestCase):
@@ -9,3 +10,25 @@ class TestBruteForceInversions(unittest.TestCase):
 
     def test_brute_inversions2(self):
         self.assertEqual(brute_force_inversions([5, 4, 3, 2, 1]), 10)
+
+    def test_brute_inversions3(self):
+        x = list(range(1000))
+        y = list(reversed(x))
+        n_invs = 1000*999//2
+        self.assertEqual(brute_force_inversions(y), n_invs)
+
+
+class TestSortCountInv(unittest.TestCase):
+    def test_sort_and_count_inv(self):
+        self.assertEqual(sort_and_count_inv([1, 3, 5, 2, 4, 6]),
+                         ([1, 2, 3, 4, 5, 6], 3))
+
+    def test_sort_and_count_inv2(self):
+        self.assertEqual(sort_and_count_inv([5, 4, 3, 2, 1]),
+                         ([1, 2, 3, 4, 5], 10))
+
+    def test_sort_and_count_inv3(self):
+        x = list(range(1000))
+        y = list(reversed(x))
+        n_invs = 1000*999//2
+        self.assertEqual(sort_and_count_inv(y), (x, n_invs))
